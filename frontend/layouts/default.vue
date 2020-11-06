413cfe7aeb50aad86f3d80f27fe7d4dd4f760881
Alexander Debarbara <alexander.silva.prs@synergiaconsultoria.com.br>
2020-11-06 20:19:21 -0300

    Initial

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app id="inspire">
+    <toolbar :state="layout"/>
+    <sidenav-left :state="layout"/>
+    <v-content>
+      <v-container fluid>
+        <nuxt></nuxt>
+      </v-container>
+    </v-content>
+    <le-footer/>
+    <v-snackbar
+      :timeout="snack.timeout"
+      :color="snack.color"
+      bottom
+      v-model="snack.visible"
+    >
+      {{snack.text}}
+      <v-btn dark flat @click.native="snack.visible = false">Close</v-btn>
+    </v-snackbar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data: () => ({
+      layout: {
+        drawer: true,
+      },
+    }),
+    computed: {
+      snack () {
+        return this.$store.getters.snack
+      }
+    }
+  }
+</script>
